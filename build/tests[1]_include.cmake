73b27762553815f5ba88a95bcd8e3587bc27d410
taimouteo <timodoeswijk@hotmail.com>
2025-06-13 03:09:06 +0000

    Borre basura

--- build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/workspaces/tp-codigo/build/tests[1]_tests.cmake")
-  include("/workspaces/tp-codigo/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
